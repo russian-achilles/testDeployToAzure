370495289967b00b80ed84ab5c0efcd7c578062d
maruyama_ta <maruyama_ta@mti.co.jp>
2020-06-15 17:13:05 +0900

    [update] test commit
    CI動くかテスト

--- FY2020TrainingBoshimo/Program.cs
+++ FY2020TrainingBoshimo/Program.cs
@@ -21,6 +21,7 @@ namespace FY2020TrainingBoshimo
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                }
+                );
     }
 }
