689598cff88db2f511ac2500db7479c265ec732a
maruyama_ta <maruyama_ta@mti.co.jp>
2020-06-15 15:00:55 +0900

    プロジェクト ファイルを追加します。

---
+++ FY2020TrainingBoshimo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FY2020TrainingBoshimo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
